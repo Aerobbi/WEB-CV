224feb945d3fbcaf4a5b500aa2183bc76c16f90f
Aerobi Sabdha <aerobbi.131@gmail.com>
2020-07-14 03:51:46 +0700

    Add files via upload

---
+++ Submission Web/assets/over.js
@@ -0,0 +1,14 @@
+const klik1 = document.querySelector('.url1');
+klik1.addEventListener('click', function(){
+    alert('Silahkan buka Email dan kirimkan pesan');
+});
+
+const klik2 = document.querySelector('.url2');
+klik2.addEventListener('click', function(){
+    alert('Langsung aja ke whatsapp');
+});
+
+const klik3 = document.querySelector('.url3');
+klik3.addEventListener('click', function(){
+    alert('Follow instagramku');
+});
